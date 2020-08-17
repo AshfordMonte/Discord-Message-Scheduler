761890491291a1790d394acba8791b4cf8400458
AshfoMon <ashfordmonte@gmail.com>
2020-08-17 14:59:12 -0500

    Removed node modules

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const client = new Discord.Client();
 const config = require('./config.json');
 
 
+
 client.once('ready', () => {
   console.log('Ready!');
 });
 
+// Login to discord using app token
 client.login(config.token || process.env.TOKEN);
