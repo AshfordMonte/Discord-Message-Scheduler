5303fa45149092d0343f3923c8b89f00e029c42b
AshfoMon <ashfordmonte@gmail.com>
2020-08-17 18:42:16 -0500

    Added custom messages and implemented designated times

--- index.js
+++ index.js
@@ -1,56 +1,85 @@
 require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
-// Cron is used to schedule tasks easily
 const cron = require('cron');
 var CronJob = cron.CronJob;
-
-// const job = cron.job('30 4 * * Fri,Sat,Tue');
-// console.log(job.nextDates(5).map((date) => date.toString()));
 
 client.once('ready', () => {
   console.log('Ready!');
 });
 
-var job = new CronJob(
-  '30 17 * * *',
+var training30HF = new CronJob(
+  '30 17 * * Fri,Tue',
   function () {
-    // console.log('Function called');
     const channel = client.channels.cache.get('745008014758772776');
-    channel.send('Training in 30 minutes! @everyone');
+    channel.send('Holdfast Training in 30 minutes! Join teamspeak by then @everyone');
   },
   null,
   true,
   'America/Chicago'
 );
 
-var job2 = new CronJob(
-  '00 18 * * *',
+var trainingHF = new CronJob(
+  '00 18 * * Fri,Tue',
   function () {
-    // console.log('Function called');
     const channel = client.channels.cache.get('745008014758772776');
-    channel.send('Training has started, hop in teamspeak! @everyone');
+    channel.send('Holdfast Training has started! Join teamspeak @everyone');
   },
   null,
   true,
   'America/Chicago'
 );
 
-var job3 = new CronJob(
-  '00 19 * * *',
+var eventHF = new CronJob(
+  '00 19 * * Fri,Tue',
   function () {
-    // console.log('Function called');
     const channel = client.channels.cache.get('745008014758772776');
-    channel.send('Event has started, hop in teamspeak! @everyone');
+    channel.send('Holdfast Event has started! Join teamspeak @everyone');
   },
   null,
   true,
   'America/Chicago'
 );
 
-job.start();
-job2.start();
-job3.start();
+var training30NW = new CronJob(
+  '30 17 * * Sat',
+  function () {
+    const channel = client.channels.cache.get('745008014758772776');
+    channel.send('Warband NW Training in 30 minutes! Join teamspeak by then @everyone');
+  },
+  null,
+  true,
+  'America/Chicago'
+);
+
+var trainingNW = new CronJob(
+  '00 18 * * Sat',
+  function () {
+    const channel = client.channels.cache.get('745008014758772776');
+    channel.send('Warband NW Training has started! Join teamspeak @everyone');
+  },
+  null,
+  true,
+  'America/Chicago'
+);
+
+var eventNW = new CronJob(
+  '00 19 * * Sat',
+  function () {
+    const channel = client.channels.cache.get('745008014758772776');
+    channel.send('Warband NW Event has started! Join teamspeak @everyone');
+  },
+  null,
+  true,
+  'America/Chicago'
+);
+
+training30HF.start();
+trainingHF.start();
+eventHF.start();
+training30NW.start();
+trainingNW.start();
+eventNW.start();
 
 // Login to discord using app token, defaults to process.env.DISCORD_TOKEN
 client.login();
