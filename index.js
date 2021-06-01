4cf96c6737c9917457d3ead7c73b07df2a43ad70
AshfoMon <ashfordmonte@gmail.com>
2021-06-01 15:24:10 -0500

    enabled saturday reminder

--- index.js
+++ index.js
@@ -83,29 +83,29 @@ var eventHF = new CronJob(
   }, null, true, 'America/Chicago'
 );
 
-// var training30NW = new CronJob(
-//   '30 17 * * Sat',
-//   function () {
-//     const channel = client.channels.cache.get(id);
-//     channel.send('Warband NW Training in 30 minutes! Join teamspeak by then @everyone');
-//   }, null, true, 'America/Chicago'
-// );
+var training30NW = new CronJob(
+  '30 17 * * Sat',
+  function () {
+    const channel = client.channels.cache.get(id);
+    channel.send('Warband NW Training in 30 minutes! Join teamspeak by then @everyone');
+  }, null, true, 'America/Chicago'
+);
 
-// var trainingNW = new CronJob(
-//   '00 18 * * Sat',
-//   function () {
-//     const channel = client.channels.cache.get(id);
-//     channel.send('Warband NW Training has started! Join teamspeak @everyone');
-//   }, null, true, 'America/Chicago'
-// );
+var trainingNW = new CronJob(
+  '00 18 * * Sat',
+  function () {
+    const channel = client.channels.cache.get(id);
+    channel.send('Warband NW Training has started! Join teamspeak @everyone');
+  }, null, true, 'America/Chicago'
+);
 
-// var eventNW = new CronJob(
-//   '00 19 * * Sat',
-//   function () {
-//     const channel = client.channels.cache.get(id);
-//     channel.send('Warband NW Event has started! Join teamspeak @everyone');
-//   }, null, true, 'America/Chicago'
-// );
+var eventNW = new CronJob(
+  '00 19 * * Sat',
+  function () {
+    const channel = client.channels.cache.get(id);
+    channel.send('Warband NW Event has started! Join teamspeak @everyone');
+  }, null, true, 'America/Chicago'
+);
 
 var eventReminder = new CronJob(
   '15 18 * * Fri',
